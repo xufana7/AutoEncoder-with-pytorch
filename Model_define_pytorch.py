9ffdfb9515aac616e9c95833b1c8c618760d61c1
xufana7 <1778366330@qq.com>
2020-08-31 09:38:05 +0800

    Update Model_define_pytorch.py

--- Model_define_pytorch.py
+++ Model_define_pytorch.py
@@ -14,6 +14,89 @@ from torch.utils.data import Dataset
 
 # This part implement the quantization and dequantization operations.
 # The output of the encoder must be the bitstream.
+def Num2Bit(Num, B):
+    Num_ = Num.type(torch.uint8)
+
+    def integer2bit(integer, num_bits=B * 2):
+        dtype = integer.type()
+        exponent_bits = -torch.arange(-(num_bits - 1), 1).type(dtype)
+        exponent_bits = exponent_bits.repeat(integer.shape + (1,))
+        out = integer.unsqueeze(-1) // 2 ** exponent_bits
+        return (out - (out % 1)) % 2
+
+    bit = integer2bit(Num_)
+    bit = (bit[:, :, B:]).reshape(-1, Num_.shape[1] * B)
+    return bit.type(torch.float32)
+
+
+def Bit2Num(Bit, B):
+    Bit_ = Bit.type(torch.float32)
+    Bit_ = torch.reshape(Bit_, [-1, int(Bit_.shape[1] / B), B])
+    num = torch.zeros(Bit_[:, :, 1].shape).cuda()
+    for i in range(B):
+        num = num + Bit_[:, :, i] * 2 ** (B - 1 - i)
+    return num
+
+
+class Quantization(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, x, B):
+        ctx.constant = B
+        step = 2 ** B
+        out = torch.round(x * step - 0.5)
+        out = Num2Bit(out, B)
+        return out
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        # return as many input gradients as there were arguments.
+        # Gradients of constant arguments to forward must be None.
+        # Gradient of a number is the sum of its four bits.
+        b, _ = grad_output.shape
+        grad_num = torch.sum(grad_output.reshape(b, -1, ctx.constant), dim=2)
+        return grad_num, None
+
+
+class Dequantization(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, x, B):
+        ctx.constant = B
+        step = 2 ** B
+        out = Bit2Num(x, B)
+        out = (out + 0.5) / step
+        return out
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        # return as many input gradients as there were arguments.
+        # Gradients of non-Tensor arguments to forward must be None.
+        # repeat the gradient of a Num for four time.
+        b, c = grad_output.shape
+        grad_output = grad_output.unsqueeze(2) / ctx.constant
+        grad_bit = grad_output.expand(b, c, ctx.constant)
+        return torch.reshape(grad_bit, (-1, c * ctx.constant)), None
+
+
+class QuantizationLayer(nn.Module):
+
+    def __init__(self, B):
+        super(QuantizationLayer, self).__init__()
+        self.B = B
+
+    def forward(self, x):
+        out = Quantization.apply(x, self.B)
+        return out
+
+
+class DequantizationLayer(nn.Module):
+
+    def __init__(self, B):
+        super(DequantizationLayer, self).__init__()
+        self.B = B
+
+    def forward(self, x):
+        out = Dequantization.apply(x, self.B)
+        return out
 
 
 def conv3x3(in_planes, out_planes, stride=1):
