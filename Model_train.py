0eabda93d821ea33dd2a6f1900eceec5ec6d7bd9
xufana7 <1778366330@qq.com>
2020-09-02 15:08:43 +0800

    Add files via upload

---
+++ Model_train.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python3
+"""An Implement of an autoencoder with pytorch.
+This is the template code for 2020 NIAC https://naic.pcl.ac.cn/.
+The code is based on the sample code with tensorflow for 2020 NIAC and it can only run with GPUS.
+If you have any questions, please contact me with https://github.com/xufana7/AutoEncoder-with-pytorch
+Author, Fan xu Aug 2020
+"""
+import numpy as np
+import h5py
+import torch
+from Model_define_pytorch import AutoEncoder, DatasetFolder
+import os
+import torch.nn as nn
+
+# Parameters for training
+os.environ["CUDA_VISIBLE_DEVICES"] = '0'
+use_single_gpu = True  # select whether using single gpu or multiple gpus
+torch.manual_seed(1)
+batch_size = 512
+epochs = 1000
+learning_rate = 1e-3
+num_workers = 4
+print_freq = 100  # print frequency (default: 60)
+# parameters for data
+feedback_bits = 128
+img_height = 16
+img_width = 32
+img_channels = 2
+
+# Model construction
+model = AutoEncoder(feedback_bits)
+if use_single_gpu:
+    model = model.cuda()
+
+else:
+    # DataParallel will divide and allocate batch_size to all available GPUs
+    autoencoder = torch.nn.DataParallel(model).cuda()
+
+criterion = nn.MSELoss().cuda()
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+# Data loading
+data_load_address = './data'
+mat = h5py.File(data_load_address + '/H_train.mat', 'r')
+data = np.transpose(mat['H_train'])  # shape=(320000, 1024)
+data = data.astype('float32')
+data = np.reshape(data, [len(data), img_channels, img_height, img_width])
+np.random.shuffle(data)
+start = int(data.shape[0] * 0.7)
+x_train, x_test = data[:start], data[start:]
+
+# dataLoader for training
+train_dataset = DatasetFolder(x_train)
+train_loader = torch.utils.data.DataLoader(
+    train_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers, pin_memory=True)
+
+# dataLoader for training
+test_dataset = DatasetFolder(x_test)
+test_loader = torch.utils.data.DataLoader(
+    test_dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers, pin_memory=True)
+best_loss = 1
+for epoch in range(epochs):
+    # model training
+    model.train()
+    for i, input in enumerate(train_loader):
+        # adjust learning rate
+        if epoch == 300:
+            for param_group in optimizer.param_groups:
+                param_group['lr'] = learning_rate * 0.1
+        input = input.cuda()
+        # compute output
+        output = model(input)
+        loss = criterion(output, input)
+        # compute gradient and do Adam step
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        if i % print_freq == 0:
+            print('Epoch: [{0}][{1}/{2}]\t'
+                  'Loss {loss:.4f}\t'.format(
+                epoch, i, len(train_loader), loss=loss.item()))
+    # model evaluating
+    model.eval()
+    total_loss = 0
+    with torch.no_grad():
+        for i, input in enumerate(test_loader):
+            input = input.cuda()
+            output = model(input)
+            total_loss += criterion(output, input).item() * input.size(0)
+        average_loss = total_loss / len(test_dataset)
+        if average_loss < best_loss:
+            # model save
+            # save encoder
+            modelSave1 = './Modelsave/encoder.pth.tar'
+            torch.save({'state_dict': model.encoder.state_dict(), }, modelSave1)
+            # save decoder
+            modelSave2 = './Modelsave/decoder.pth.tar'
+            torch.save({'state_dict': model.decoder.state_dict(), }, modelSave2)
+            print("Model saved")
+            best_loss = average_loss
